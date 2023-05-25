20006358f652c13102c1a2aa482348cf7643d4a0
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-25 12:11:05 +0200

    added an event listener to my queueJoin, leaveQueue, and taxiJoin buttons and incremented the counter variable in the UI

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -5,11 +5,25 @@
 // passangers element references
 const queueJoin = document.querySelector('.join_queue');
 const counter = document.querySelector('.count');
-const leaveQueue = document.querySelector('.leave_queue');
+const leaveQueue = document.querySelector('.leave_queue'); 
 
+// taxi section elements references
+const taxiJoin = document.querySelector('.join_taxi_queue');
+const taxiCounter = document.querySelector('.taxi_queue_count');
 // create Factory Function instance
 
 const taxiQueue = TaxiQueue();
 
 // DOM events
+queueJoin.addEventListener('click', function() {
+    counter.innerText = taxiQueue.joinQueue();
+});
 
+
+leaveQueue.addEventListener('click', function() {
+    counter.innerText = taxiQueue.leaveQueue();
+});
+
+taxiJoin.addEventListener('click', function() {
+    taxiCounter.innerText = taxiQueue.joinTaxiQueue();
+});
