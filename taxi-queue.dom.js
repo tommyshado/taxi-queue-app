7e55d443a771e22c05e96f26fde2f05a2d8700a5
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-25 10:43:58 +0200

    got references to the passangers element references

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -2,6 +2,10 @@
 
 
 // DOM element references
+// passangers element references
+const queueJoin = document.querySelector('.join_queue');
+const counter = document.querySelector('.count');
+const leaveQueue = document.querySelector('.leave_queue');
 
 // create Factory Function instance
 
